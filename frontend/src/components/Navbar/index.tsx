39d3cf61c438e3d58c2be68356d5b53291e6b9fc
Phillip Marques <phillipmarq@hotmail.com>
2022-01-10 10:40:30 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as Github } from 'assets/img/github.svg';
+import './styles.css';
+
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovieph-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/pHMqZ">
+                        <div className='dsmovieph-contact-container'>
+                            <Github />
+                            <p className='dsmovieph-contact-link'>/pHMqZ</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
